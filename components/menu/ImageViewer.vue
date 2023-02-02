ab520ec048e5ce288df30d739a31110c03b698e1
Saurav Kumal <sk.sauravkumal@gmail.com>
2023-02-02 12:30:03 +0545

    image viewer added

---
+++ components/menu/ImageViewer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-img :width="300" contain :src="imageUrl"/>
+</template>
+
+<script>
+export default {
+  name: "ImageViewer",
+  props: ['image', 'url'],
+
+  computed: {
+    imageUrl() {
+      if (this.image) {
+        return URL.createObjectURL(this.image)
+      } else if (this.url) {
+        return this.url
+      } else {
+        return 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
