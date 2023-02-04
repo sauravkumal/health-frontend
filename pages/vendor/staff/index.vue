c9693f1a8d464d1330e7c3466f04679609637e7c
Saurav Kumal <sk.sauravkumal@gmail.com>
2023-02-04 12:21:28 +0545

    table

--- pages/vendor/staff/index.vue
+++ pages/vendor/staff/index.vue
@@ -1,56 +1,55 @@
 <template>
-  <v-card elevation="4">
-    <v-data-table
-      dense
-      :hide-default-footer="true"
-      :headers="headers"
-      :items="items"
-      :items-per-page="5"
-      :loading="loading"
-      :server-items-length="total"
-      :options.sync="options"
-      selectable-key="id"
-      show-select
-      :sort-desc="true">
+  <v-card flat>
+    <v-card-title class="tw-justify-between">
+      <div>
+        <v-text-field
+          hide-details
+          label="Search.."
+          single-line
+          clearable
+          dense
+          outlined
+          prepend-inner-icon="mdi-magnify"
+          v-model:trim="search"></v-text-field>
+      </div>
+      <v-btn color="primary" icon @click="fetch">
+        <v-icon>mdi-refresh</v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-card-text>
+      <v-data-table
+        dense
+        :hide-default-footer="true"
+        :headers="headers"
+        :items="items"
+        :items-per-page="5"
+        :loading="loading"
+        :server-items-length="total"
+        :options.sync="options"
+        :sort-desc="true">
 
-      <template v-slot:top="{pagination, options, updateOptions}">
-        <v-toolbar dense>
-          <v-toolbar-items class="tw-w-full tw-gap-1">
-            <v-text-field
-              hide-details
-              label="Search"
-              single-line
-              class="mt-2"
-              clearable
-              prepend-inner-icon="mdi-magnify"
-              v-model:trim="search"></v-text-field>
+        <template v-slot:top="{pagination, options, updateOptions}">
+          <v-data-footer
+            :pagination="pagination"
+            :options="options"
+            @update:options="updateOptions"
+            items-per-page-text="$vuetify.dataTable.itemsPerPageText"/>
+        </template>
 
-            <v-spacer/>
-            <v-btn color="primary" icon @click="fetch">
-              <v-icon>mdi-refresh</v-icon>
-            </v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-        <v-data-footer
-          :pagination="pagination"
-          :options="options"
-          @update:options="updateOptions"
-          items-per-page-text="$vuetify.dataTable.itemsPerPageText"/>
-      </template>
+        <template v-slot:item.actions={item}>
+          <v-btn text small link color="primary" @click="resend(item.id)">Resend Email</v-btn>
+          <v-btn icon color="error"
+                 @click="destroy(item.id)">
+            <v-icon small>mdi-delete</v-icon>
+          </v-btn>
+        </template>
 
-      <template v-slot:item.actions={item}>
-        <v-btn text small link color="primary" @click="resend(item.id)">Resend Email</v-btn>
-        <v-btn icon color="error"
-               @click="destroy(item.id)">
-          <v-icon small>mdi-delete</v-icon>
-        </v-btn>
-      </template>
-
-      <template v-slot:item.created_at={item}>
-        {{ $moment(item.created_at).format('YYYY-MM-DD hh:mm A') }}
-        <span class="tw-ml-4 tw-text-xs">{{ $moment(item.created_at).fromNow() }}</span>
-      </template>
-    </v-data-table>
+        <template v-slot:item.created_at={item}>
+          {{ $moment(item.created_at).format('YYYY-MM-DD hh:mm A') }}
+          <span class="tw-ml-4 tw-text-xs">{{ $moment(item.created_at).fromNow() }}</span>
+        </template>
+      </v-data-table>
+    </v-card-text>
   </v-card>
 </template>
 
