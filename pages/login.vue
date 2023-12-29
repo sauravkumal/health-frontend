5e49d6a6511c3e54f0ea370fe04a9667b89bd041
sauravkumal <sk.sauravkumal1@gmail.com>
2023-12-30 02:26:53 +0545

    update icon

--- pages/login.vue
+++ pages/login.vue
@@ -1,41 +1,38 @@
 <template>
   <v-container class="fill-height justify-center">
-    <v-card elevation="4" class="tw-w-full sm:tw-w-1/2 md:tw-w-2/5">
-      <v-card-title class="justify-center">
-        <v-avatar size="100">
-          <v-icon>mdi-account</v-icon>
-        </v-avatar>
+    <v-card flat outlined class="tw-w-full sm:tw-w-1/2 md:tw-w-1/3">
+      <v-card-title class="justify-center tw-my-4">
+        <div class="tw-flex tw-items-center tw-flex-col">
+          <v-img height="100" width="100" contain src="/healthcare.png" alt="logo"/>
+        </div>
+
       </v-card-title>
       <v-card-text>
         <v-form @submit.prevent="login">
-          <ValidationObserver ref="loginValidator">
-            <ValidationProvider name="Email" vid="email" rules="required|email" v-slot="{errors}">
+          <ValidationObserver ref="loginValidator" tag="div" class="tw-flex tw-flex-col tw-gap-4">
+            <ValidationProvider tag="div" name="Email" vid="email" rules="required|email" v-slot="{errors}">
               <v-text-field label="Email"
                             v-model:trim="email"
+                            dense
+                            outlined
                             prepend-inner-icon="mdi-email"
                             :error-messages="errors"/>
             </ValidationProvider>
-            <ValidationProvider vid="password" name="Password" rules="required" v-slot="{errors}">
-
+            <ValidationProvider tag="div" vid="password" name="Password" rules="required" v-slot="{errors}">
               <v-text-field v-model="password"
                             label="Password"
+                            dense
+                            outlined
                             :type="showPassword ? 'text' : 'password'"
                             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                             @click:append="showPassword = !showPassword"
                             :error-messages="errors"
                             prepend-inner-icon="mdi-lock"/>
             </ValidationProvider>
+
+            <v-btn depressed type="submit" class="tw-mt-2" :loading="loggingIn" block color="primary">Login</v-btn>
           </ValidationObserver>
-
-          <v-btn type="submit" :loading="loggingIn" block color="primary">Login</v-btn>
-
         </v-form>
-        <div class="tw-flex tw-justify-center tw-mt-2">
-          <NuxtLink to="/forgotpassword">
-            Forgot Password
-          </NuxtLink>
-        </div>
-
       </v-card-text>
     </v-card>
   </v-container>
